f702bed680602cb71e19a8ffccf70205a614db13
BeomB <qjarl1119@naver.com>
2023-10-08 23:06:47 +0900

    진행 중 - 색션2 완료

---
+++ src/main/java/beom/servlet/basic/request/RequestParamServlet.java
@@ -0,0 +1,19 @@
+package beom.servlet.basic.request;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "requestParamSerclet",urlPatterns = "/request-param")
+public class RequestParamServlet extends HttpServlet {
+
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("RequestParamServlet.service");
+        req.getParameterNames().asIterator().forEachRemaining(paramName-> System.out.println(paramName+"=" + req.getParameter(paramName)));
+    }
+}
