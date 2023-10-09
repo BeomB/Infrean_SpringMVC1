4859706906ea896701974934d8c4479c255b120d
BeomB <qjarl1119@naver.com>
2023-10-09 21:32:09 +0900

    색션 3 서블릿, JSP, MVC 패턴 완료

---
+++ src/main/java/beom/servlet/web/frontcontroller/v1/MemberFromControllerV1.java
@@ -0,0 +1,14 @@
+package beom.servlet.web.frontcontroller.v1;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MemberFromControllerV1 implements ControllerV1{
+
+    @Override
+    public void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
